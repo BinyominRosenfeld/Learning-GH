e6f97e08cf8ca2b5c1792cfb4e8b558c5d6c53b9
Binyomin Rosenfeld <ben.m.rosenfeld@gmail.com>
2022-04-07 22:20:17 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Binyomin Rosenfeld on 07/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
